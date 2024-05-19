f7f4f64db6c296b45821e46acb92b89b5dc96fca
Fang <74826581+mn1shi@users.noreply.github.com>
2024-05-20 00:47:37 +0100

    JionButton

---
+++ Assets/Scripts/JoinSameRoom.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Ubiq.Rooms;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
+using System.Net.Mail;
+using Ubiq.Samples;
+
+public class JoinSameRoom : MonoBehaviour
+{
+    public SocialMenu mainMenu;
+    public TMP_Text joincodeText;
+    private string lastRequestedJoincode;
+
+
+    public void Join()
+    {
+        lastRequestedJoincode = joincodeText.text.ToLowerInvariant();
+        mainMenu.roomClient.Join(joincode: lastRequestedJoincode);
+    }
+
+}
